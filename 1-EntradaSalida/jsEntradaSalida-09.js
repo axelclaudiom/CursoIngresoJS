4b3274855a1b71ea3f661a319856450e8571c6a2
axelclaudiom <axelclaudiom@gmail.com>
2021-07-06 15:54:07 -0300

    Ejercicios_Realizados

--- 1-EntradaSalida/jsEntradaSalida-09.js
+++ 1-EntradaSalida/jsEntradaSalida-09.js
@@ -1,10 +1,12 @@
-/*
+/*Melgar Claudio Axel Div H
+E/S nº 9
 Debemos lograr tomar el importe por ID ,
 transformarlo a entero (parseInt), luego
 mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
-
-	alert("ok");
+	sueldo=parseInt(txtIdSueldo.value);
+	resultado = (sueldo*10/100)+sueldo;
+	txtIdResultado.value=resultado;
 }
--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -1,9 +1,15 @@
-/*
+/* Melgar Claudio Axel Div H
+E/S nº 10
 Debemos lograr tomar el importe por ID.
 Transformarlo a entero (parseInt), luego
 mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
-	alert("ok");
+	importe=parseInt(txtIdImporte.value);
+	resultado = importe - (importe*25/100);
+	txtIdResultado.value=resultado;
+
+	//txtIdImporte
+	//txtIdResultado
 }
