63adfb1c69a118bb9afca2ae42ad18928a5521ef
axelclaudiom <axelclaudiom@gmail.com>
2021-07-06 13:22:59 -0300

    ejercicios_hechos

--- 1-EntradaSalida/jsEntradaSalida-07.js
+++ 1-EntradaSalida/jsEntradaSalida-07.js
@@ -1,25 +1,38 @@
-/*
+/* Melgar Claudio Axel Div H
+E/S n 7
 Debemos lograr tomar Los numeros por ID ,jkj
 transformarlos a enteros (parseInt),realizar la operación correcta y 
 mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
 {	
-	alert("ok");	
+	let n1 = parseInt(txtIdNumeroUno.value);
+	let n2 = parseInt(txtIdNumeroDos.value);
+	let total = n1 + n2;
+	alert("La suma es: " + total);
 }
 
 function restar()
 {
-	
+	let n1 = parseInt(txtIdNumeroUno.value);
+	let n2 = parseInt(txtIdNumeroDos.value);
+	let total = n1 - n2;
+	alert("La resta es: " + total);
 }
 
 function multiplicar()
 { 
-	
+	let n1 = parseInt(txtIdNumeroUno.value);
+	let n2 = parseInt(txtIdNumeroDos.value);
+	let total = n1 * n2;
+	alert("La multiplicación es: " + total);
 }
 
 function dividir()
 {
-	
+	let n1 = parseInt(txtIdNumeroUno.value);
+	let n2 = parseInt(txtIdNumeroDos.value);
+	let total = n1 / n2;
+	alert("La división es: " + total);
 }
 
--- 1-EntradaSalida/jsEntradaSalida-08.js
+++ 1-EntradaSalida/jsEntradaSalida-08.js
@@ -1,9 +1,16 @@
-/*
+/* Melgar Claudio Axel Div H
+E/S 8
 Debemos lograr tomar Los numeros por ID ,
 transformarlos a enteros (parseInt),realizar la operación correcta y 
 mostrar el resto entre el dividendo y el divisor.
-ej.: "El resto es 0 ."*/
+ej.: "El resto es 0 ." %*/ 
 function SacarResto()
 {
-	alert("ok");
+	let n1 = parseInt(txtIdNumeroDividendo.value);
+	let n2 = parseInt(txtIdNumeroDivisor.value);
+	let total = n1 % n2;
+	alert("El resto es: " + total);
+	//alert("ok");
+	//txtIdNumeroDividendo
+	//txtIdNumeroDivisor
 }
