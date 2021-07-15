222f6c2ae96617342ed516503070cd38dfc4a379
axelclaudiom <axelclaudiom@gmail.com>
2021-07-15 20:14:03 -0300

    Ejercicios_Realizados

--- 3-InstruccionSwitch/jsInstruccionSwitch-01.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-01.js
@@ -1,9 +1,33 @@
+/*si es Enero: "que comiences bien el año!!!."
+si es Marzo: "a clases!!!."
+si es Julio: "se vienen las vacaciones!!!."
+si es Diciembre: "Felices fiesta!!!." */
 function mostrar()
 {
 	//tomo el mes
-	var mesDelAño =txtIdMes.value;
+	var mesDelAño; 
+	let mensaje;
+	mesDelAño = txtIdMes.value;
 	alert(mesDelAño);
-
+	switch(mesDelAño)
+	{
+		case "Enero":
+			mensaje = "que comiences bien el año!!!.";
+		break;
+		case "Marzo":
+			mensaje = "a clases!!!.";
+		break;
+		case "Julio":
+			mensaje = "se vienen las vacaciones!!!.";
+		break;
+		case "Diciembre":
+			mensaje = "Felices fiesta!!!.";
+		break;
+		default:
+			mensaje = "No hay nada especial en este mes";
+		break;
+	}
+	alert(mensaje);
 
 
 }//FIN DE LA FUNCIÓN
--- 3-InstruccionSwitch/jsInstruccionSwitch-02.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-02.js
@@ -1,9 +1,28 @@
 function mostrar()
+
+/*al seleccionar un mes informar.
+si estamos en Invierno: "Abrigate que hace frio."
+si aún no llego el Invierno: "Falta para el invierno."
+si ya paso el Invierno: "Ya pasamos el frio, ahora calor!!!."
+ACLARAcIÓN: tomamos a Julio y Agosto como los meses de Invierno. 
+Julio y agosto -> invierno "Abrigate que hace frio."
+septiem - oct - nov -> primavera "Ya pasamos el frio, ahora calor!!!."
+dic - ene - feb -> verano "Ya pasamos el frio, ahora calor!!!."
+marzo - abril - mayo - junio -> inv "Falta para el invierno."
+*/
 {
 	//tomo el mes
 	var mesDelAño =txtIdMes.value;
 	alert(mesDelAño);
-
+	switch(mesDelAño)
+	{
+		case "Marzo":
+		case "Abril":
+		case "Mayo":
+		case "Junio":
+			mensaje = "Falta para el invierno.";
+		break;
+	}
 
 
 
