587c03caf3356e5ed852983f35e4caeab8bfb83a
axelclaudiom <axelclaudiom@gmail.com>
2021-07-26 11:42:41 -0300

    Ejercicios_Realizados

--- 9-Parciales/2020_1Cuatri_parcial_fuentes/tres.js
+++ 9-Parciales/2020_1Cuatri_parcial_fuentes/tres.js
@@ -1,4 +1,16 @@
 function mostrar()
 {
-	alert("tres");
+	var tipoProducto;
+	var precioProducto;
+	var cantidadUnidades;
+	var cantProductosIngr; //cuenta la cantidad de productos ingresados hasta el momento
+	alert("Ingrese 5 productos");
+	while(cantProductosIngr < 6)
+	{
+		tipoProducto = prompt("Ingrese el tipo de producto (barbijo, jabon, alcohol)");
+		precioProducto = prompt("Ingrese el precio del producto");
+		cantidadUnidades = prompt("Ingrese la cantidad de unidades del producto ingresado");
+		cantProductosIngr += 1;
+	}
+	console.log(cantProductosIngr);
 }
--- 9-Parciales/2020_1Cuatri_parcial_fuentes/uno.js
+++ 9-Parciales/2020_1Cuatri_parcial_fuentes/uno.js
@@ -1,5 +1,38 @@
-
 function mostrar()
 {
-	alert("uno");
+	var tipoProducto;
+	var precioProducto;
+	var cantidadUnidades;
+	var cantProductosIngr; //cuenta la cantidad de productos ingresados hasta el momento
+	var fabricante;
+	var banderaAlcohol;
+	var fabricanteBarat;
+	cantProductosIngr = 0;
+	banderaAlcohol = false;
+	alcoholBarato = 0;
+	alert("Ingrese 5 productos");
+	while(cantProductosIngr < 5)
+	{
+		tipoProducto = prompt("Ingrese el tipo de producto (barbijo, jabon, alcohol)");
+		precioProducto = prompt("Ingrese el precio del producto");
+		precioProducto = parseInt(precioProducto);
+		cantidadUnidades = prompt("Ingrese la cantidad de unidades del producto ingresado");
+		cantidadUnidades = parseInt(cantidadUnidades);
+		fabricante = prompt("Ingrese la marca/fabricante del producto");
+		cantProductosIngr += 1;
+		// Del más barato de los alcohol, la cantidad de unidades y el fabricante
+		if(tipoProducto == "alcohol")
+		{
+			if(precioProducto < alcoholBarato || banderaAlcohol == false)
+			{
+				alcoholBarato = precioProducto;
+				unidadesBarat = cantidadUnidades;
+				fabricanteBarat = fabricante;
+				banderaAlcohol = true;
+				console.log("Este alcohol es una shit");
+			}
+		}
+	}
+	document.write("El alcohol mas barato cuesta: " + alcoholBarato + " Tiene " + unidadesBarat + " Unidades, el fabricante es:  " + fabricanteBarat);
+	console.log(cantProductosIngr);
 }
